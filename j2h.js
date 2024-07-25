82513f26ba718e9957531263bf6232e27f78fcc6
Chad Brown <github@crystallinetech.com>
2024-07-24 21:36:41 -0700

    Add License
    Added MIT license
    Check for components that are already loaded

--- j2h.js
+++ j2h.js
@@ -1,6 +1,9 @@
 
-//j2h-framework for the browser (client)
-// requires json2html (2.1.0+), jquery (1.9+), page.js (1.8.5+)
+//     j2h-framework.js 1.1.0
+//     https://www.json2html.com
+//     (c) 2006-2024 Crystalline Technologies
+//     j2h-framework may be freely distributed under the MIT license.
+//     requires json2html (3.1.0+), page.js (1.8.5+)
 
 var j2h = {
     
@@ -71,7 +74,7 @@ var j2h = {
         //Set the pop state
         window.onpopstate = function(e) {
           page.replace(e.state.path);
-        }
+        };
     }
 };
 
@@ -79,42 +82,47 @@ j2h.Obj = class {
     
     constructor() {}
     
-    //Get components associated with the object (Page or Component)
-    async getComponents(){
+    //Get the components for this object
+    // add them to the global json2html components
+    async setComponents(){
         
         let base = this;
         
-        //Initialize the component store
-        let _components = {};
-        
-        if(!base.components) return(_components);
+        if(!base.components) return;
         
         //Itterate over the components for this page
         for(let _name in base.components) {
             
-            let _path = base.components[_name];
+            let _path = base.components[_name],
+                _component;
             
-            try {
-                //Load the script from the path
-                await j2h._load(_path);
-            } catch(e) {
-                continue;
+            //Load the component if we don't already have it
+            // in json2html
+            if(!json2html.component.get(_name)) {
+            
+                try {
+                    //Load the script from the path
+                    await j2h._load(_path);
+                } catch(e) {
+                    continue;
+                }
+                
+                //Output the component error
+                if(!j2h._exports[_path]) {
+                    console.error("Unable to load component ",_name,_path,".  Did you use j2h.export?");
+                    continue;
+                }
+                
+                //Create a new instance of this component
+                _component = new j2h._exports[_path]();
+                
+                //Add the component template to json2html
+                json2html.component.add(_name,_component.template);
+                
+                //Get the sub components used by this component
+                await _component.setComponents();
             }
-            
-            //Output the component error
-            if(!j2h._exports[_path]) {
-                console.error("Unable to load component ",_name,_path," (Missing component, did you use j2h.export?)");
-                continue;
-            }
-            
-            //Add this component instance
-            _components[_name] = new j2h._exports[_path]();
-            
-            //Get the sub components used by this component
-            Object.assign(_components,await _components[_name].getComponents());
         }
-        
-        return(_components);
     }
     
 };
@@ -133,7 +141,9 @@ j2h.Page = class extends j2h.Obj {
         
         // =================== Private Properties ===================== 
         
-        //Element that we'll render to
+        //Query select OR element
+        // if ele is string we'll check for this element every time we render
+        // ensures if the element is removed/replaced then we still can find it later
         this._ele = ele;
     }
     
@@ -152,42 +162,36 @@ j2h.Page = class extends j2h.Obj {
         
         let base = this;
         
-        //Get the location we want to render this page
-        let $ele = $(base._ele);
-        
         //Get the data for this page
         // allow access to request
         let data = await base.data(req);
         
-        //Get the all the components to render this page
-        // include sub components as well
-        let components = await base.getComponents();
+        //Set the components 
+        // save them into json2html.components
+        await base.setComponents();
         
-        //Get the components as template objects
-        let _templates = base.getTemplates(components);
+        //Find the parent
+        let parent;
         
-        //Add the components to json2html
-        json2html.component.add(_templates);
-        
-        //Render the html with events (ihtml)
-        $ele.empty().json2html(data,base.template);
-    }
-    
-    // ============================= Private Methods ==================== 
-    
-    //Get the templates for these components
-    getTemplates(components) {
-        
-        let out = {};
-        
-        for(let _component in components)  {
-            let component = components[_component];
+        switch(typeof(base._ele)) {
             
-            //Add the components template if we match the client filter criteria
-            out[_component] = component.template;
+            case "string":
+                parent = document.querySelector(base._ele);
+            break;
+            
+            default:
+                parent = base._ele;
+            break;
         }
         
-        return(out);
+        //Exit if we don't have a valid parent element
+        if(!parent) return;
+        
+        //Clear the element children
+        parent.innerHTML = "";
+        
+        //Render the html with events
+        parent.json2html(data,base.template);
     }
 };
 
@@ -221,11 +225,9 @@ j2h.Request = class {
         this.protocol = this.url.protocol;
         this.params = ctx.params;
         
-        //TODO change this to a json object
-        //this.query = this.url.searchParams;
-        
+        //Get the query
         this.query = {};
-    
+        
         //Get the query
         if( window.location.href.indexOf('?') >= 0 ) {
             let hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
@@ -237,13 +239,6 @@ j2h.Request = class {
         
         //Always get
         this.method = "GET";
-        
-        //TODO
-        // this.cookies
-        // this.ip
-        // this.secure
-
-        //TODO set the state
     }
     
 };
@@ -376,11 +371,11 @@ j2h.Router = class {
                         res = new j2h.Response();
                     
                     //Render the page when  
-                    $(function(){
+                    //$(function(){
                         //Don't wait up for this to complete
                         //Render the page
                         __route.page.render(req,res);
-                    });
+                    //});
                 });
                 
             })(_route);
@@ -425,7 +420,7 @@ j2h.Router = class {
        let replace   = new RegExp(separator+'/{1,}', 'g');
        return parts.join(separator).replace(replace, separator);
     }
-}
+};
 
 //Create a new router for the default app
 j2h.app = new j2h.Router();
